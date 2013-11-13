6e305f20c3f1a6828fc2b719e39e9d2a77316e99
Sofia Larsson <karinsofiapaulina@gmail.com>
2013-11-13 08:52:09 +0100

    Reuse simple cursor loader

--- mobile/android/base/menu/MenuReadingList.java
+++ mobile/android/base/menu/MenuReadingList.java
@@ -3,6 +3,7 @@ package org.mozilla.gecko.menu;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.db.BrowserContract.Bookmarks;
 import org.mozilla.gecko.db.BrowserDB;
+import org.mozilla.gecko.home.SimpleCursorLoader;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -13,11 +14,11 @@ import android.support.v4.app.Fragment;
 import android.widget.ListView;
 import android.database.Cursor;
 import android.content.Context;
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+
 
 public class MenuReadingList extends Fragment {
 
@@ -26,64 +27,45 @@ public class MenuReadingList extends Fragment {
 
     private MenuReadingListAdapter mAdapter;
 
+    private CursorLoaderCallbacks mCursorLoaderCallbacks;
+
 	@Override
 	public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroupContainer,
 		Bundle savedInstanceState) {
-		Log.i("MyActivity", "create view");
 		View view = layoutInflater.inflate(R.layout.menu_reading_list, viewGroupContainer, false);
-		Log.i("MyActivity", "View " + view);
 		return view;
 	}
 
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {	    
-		Log.i("MyActivity", "onViewCreated before super");
 		super.onCreate(savedInstanceState);
-		Log.i("MyActivity", "onViewCreated - start");
 	    mList = (ListView) view.findViewById(R.id.list);
-
-		MenuReadingListLoader listLoader = new MenuReadingListLoader(getActivity());
-		Cursor cursor = listLoader.loadInBackground();
-		Log.i("MyActivity", "onViewCreated - middle");
-
-	    mAdapter = new MenuReadingListAdapter(getActivity(), cursor);
-        mList.setAdapter(mAdapter);
-        Log.i("MyActivity", "onViewCreated - end");
 	}
 
-	 /**
-     * Cursor loader for the list of reading list items.
-     */
-    private static class MenuReadingListLoader extends CursorLoader {
-        public MenuReadingListLoader(Context context) {
-            super(context);
-            Log.i("MyActivity", "MenuReadingListLoader Cursor loader");
-        }
+	@Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+	    
+	    mAdapter = new MenuReadingListAdapter(getActivity(), null);
+        mList.setAdapter(mAdapter);
 
-        @Override
-        public Cursor loadInBackground() {
-            return BrowserDB.getBookmarksInFolder(getContext().getContentResolver(), Bookmarks.FIXED_READING_LIST_ID);
-        }
+        mCursorLoaderCallbacks = new CursorLoaderCallbacks();
+
+        getLoaderManager().initLoader(0, null, this);
     }
 
+
     /**
-     * LoaderCallbacks implementation that interacts with the LoaderManager.
+     * Cursor loader for the list of reading list items.
      */
-    private class CursorLoaderCallbacks implements LoaderCallbacks<Cursor> {
-        @Override
-        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-            Log.i("MyActivity", "onCreateLoader Cursor loader");
-            return new MenuReadingListLoader(getActivity());
+    private static class MenuReadingListLoader extends SimpleCursorLoader {
+        public ReadingListLoader(Context context) {
+            super(context);
         }
 
         @Override
-        public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
-            mAdapter.swapCursor(c);
-        }
-
-        @Override
-        public void onLoaderReset(Loader<Cursor> loader) {
-            mAdapter.swapCursor(null);
+        public Cursor MenuloadCursor() {
+            return BrowserDB.getBookmarksInFolder(getContext().getContentResolver(), Bookmarks.FIXED_READING_LIST_ID);
         }
     }
 
@@ -97,16 +79,36 @@ public class MenuReadingList extends Fragment {
 
         @Override
         public void bindView(View view, Context context, Cursor cursor){
-        	TextView tv = (TextView) view.findViewById(R.id.reading_list_item);
+        	TextView tv = (TextView) view.findViewById(R.layout.menu_reading_list_item);
         	Log.i("MyActivity", "bindview setting textview");
-        	tv.setText("Hej hooooo");
+        	tv.setText("Test");
 
         }
 
         @Override
         public View newView(Context context, Cursor cursor, ViewGroup parent){
-        	Log.i("MyActivity", "new view, dunno what it does");
-        	return LayoutInflater.from(parent.getContext()).inflate(R.id.reading_list_item, parent, false);
+        	return LayoutInflater.from(parent.getContext()).inflate(R.layout.menu_reading_list_item, parent, false);
+        }
+    }
+
+
+    /**
+     * LoaderCallbacks implementation that interacts with the LoaderManager.
+     */
+    private class CursorLoaderCallbacks implements LoaderCallbacks<Cursor> {
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            return new MenuReadingListLoader(getActivity());
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+            mAdapter.swapCursor(c);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            mAdapter.swapCursor(null);
         }
     }
 
