9675eba271d9b549489ec6b1bf7ffed59f85c5c3
Sofia Larsson <karinsofiapaulina@gmail.com>
2013-11-15 10:57:10 +0100

    Readning list menu hidden by default

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -23,7 +23,6 @@ import org.mozilla.gecko.home.HomePager;
 import org.mozilla.gecko.home.HomePager.OnUrlOpenListener;
 import org.mozilla.gecko.home.SearchEngine;
 import org.mozilla.gecko.menu.GeckoMenu;
-import org.mozilla.gecko.menu.MenuReadingList;
 import org.mozilla.gecko.prompts.Prompt;
 import org.mozilla.gecko.util.Clipboard;
 import org.mozilla.gecko.util.GamepadUtils;
